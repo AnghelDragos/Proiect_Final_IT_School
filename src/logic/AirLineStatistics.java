098ae12ed61235b1baacd19fff3bcd8b145cf33d
DDD <dragos5dra@gmail.com>
2022-10-30 11:54:44 +0100

    Am mai lucrat ceva 9

--- src/logic/AirLineStatistics.java
+++ src/logic/AirLineStatistics.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -11,16 +12,25 @@ public class AirLineStatistics {
 
     //mai jos trebuie private, nu public
     public static String findMostUsedCityAsDepartureForFlights (AirLineManager airLineManager){
+
         List<String> collectionOfAllCityNames = airLineManager.getAllFlights().stream()
                 .map(flight -> flight.getFrom())
-                .collect(Collectors.toList());
+                .collect(Collectors.toList());//aici se face o lista cu orasele de plecare
 
-        Set<String> collectionOfSingleCityNames = airLineManager.getAllFlights().stream()
-                .map(flight -> flight.getFrom())
-                .collect(Collectors.toSet());
+        Map<String, Long> mapKeyValue = collectionOfAllCityNames.stream()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));//aici se face un map cu key==fiecare oras, valoare==numarul de plecari din orasul respectiv
 
-        System.out.println(collectionOfAllCityNames);
-        return "placeholder";
+        int MostVisitedCityValueInMap = mapKeyValue.values().stream()
+                .mapToInt(v -> Math.toIntExact(v))
+                .max().orElseThrow(NoSuchElementException::new);//aici se afla valoarea cea mai mare, adica de unde pleaca cei mai multi oameni
+
+        String mostDepartureCityName="noCityName";
+        for (Map.Entry<String, Long> entry : mapKeyValue.entrySet()) {
+            if (entry.getValue()==MostVisitedCityValueInMap) {
+                mostDepartureCityName=entry.getKey();
+            }
+        }
+        return mostDepartureCityName;
     }
 
 
